011ab527b0b73a87b3116d4330bcbc1bf45fa910
graceen95 <graceen95@gmail.com>
2020-06-12 16:09:01 -0700

    Initial commit

---
+++ hello.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template('home.html')
+
+@app.route("/hello")
+def hello():
+    return render_template('hello.html')
+
+
+if __name__ == "__main__":
+    app.run()
